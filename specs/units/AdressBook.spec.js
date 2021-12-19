37a1ad15cda6dcab56d9c8c30800da456c368fff
Elvita Kalvāne <elvita.kalvane@gmail.com>
2021-12-19 18:00:21 +0100

    add create method to address book - happy bath

--- specs/units/AdressBook.spec.js
+++ specs/units/AdressBook.spec.js
@@ -1,13 +1,64 @@
-const AddressBook = require("../../src/js/AddressBook")
+const AddressBook = require("../../src/js/AddressBook");
 
-describe('AddressBook', () => {
-  subject(() => new AddressBook())
+describe.only("AddressBook", () => {
+  afterEach(() => {
+    window.localStorage.data = {}
+    sinon.reset()
+  });
+  subject(() => new AddressBook());
 
-  it('is expected to be an Object', () => {
+  it("is expected to be an Object", () => {
     expect($subject).to.be.an("object");
   });
 
-  it('is expected to be an instance o AddressBook class', () => {
-    expect($subject).to.be.an.instanceOf(AddressBook)
+  it("is expected to be an instance o AddressBook class", () => {
+    expect($subject).to.be.an.instanceOf(AddressBook);
+  });
+
+  it(() => is.expected.to.respondTo("index"));
+  it(() => is.expected.to.respondTo("create"));
+
+  let setItemSpy, getItemSpy, stringifySpy, parseSpy, message;
+  setItemSpy = sinon.spy(window.localStorage, "setItem");
+  getItemSpy = sinon.spy(window.localStorage, "getItem");
+  stringifySpy = sinon.spy(JSON, "stringify");
+  parseSpy = sinon.spy(JSON, "parse");
+  describe("#create", () => {
+    def("contactsInStorage", () => JSON.parse(window.localStorage.data.entries));
+    def("validData", {
+      name: "John Doe",
+      email: "john@mail.com",
+      phone: "1234567",
+    });
+
+    context("with valid data", () => {
+      beforeEach(() => {
+        message = $subject.create($validData);
+      });
+
+      it("is expected to call on LocalStorage.getItem", () => {
+        expect(getItemSpy).to.have.been.calledOnce;
+      });
+
+      it("is expected to call on LocalStorage.setItem", () => {
+        expect(setItemSpy).to.have.been.calledOnce;
+      });
+
+      it("is expected to call on JSON.parse", () => {
+        expect(parseSpy).to.have.been.calledOnce;
+      });
+
+      it("is expected to call on JSON.stringify", () => {
+        expect(stringifySpy).to.have.been.calledOnce;
+      });
+
+      it('is expected to add an entry to localStorage', () => {
+        expect($contactsInStorage).to.have.length(1)
+      });
+
+      it('is expected to respond with a success message', () => {
+        expect(message).to.equal("The entry was added to the address book")
+      });
+    });
   });
 });
--- specs/units/LocalStorage.spec.js
+++ specs/units/LocalStorage.spec.js
@@ -1,6 +1,7 @@
-describe('LocalStoragge', () => {
-  
-  subject(() => window.localStorage)
+const { expect } = require("chai");
+
+describe("LocalStoragge", () => {
+  subject(() => window.localStorage);
 
   it(() => is.expected.to.respondTo("getItem"));
   it(() => is.expected.to.respondTo("setItem"));
@@ -11,23 +12,76 @@ describe('LocalStoragge', () => {
   const entry1 = {
     name: "Luize",
     phone: "1234567",
-    email: "fake@mail.com"
-  }
+    email: "fake@mail.com",
+  };
 
-  describe.only('#setItem', () => {
-    let storedData
+  describe("#setItem", () => {
+    let storedData;
 
     before(() => {
-      $subject.setItem("entries", JSON.stringify(entry1))
-      storedData = $subject.data
+      $subject.setItem("entries", JSON.stringify(entry1));
+      storedData = $subject.data;
     });
 
-    it('is expected to store data in the chosen key', () => {
-      expect(storedData).to.have.own.property("entries")
+    it("is expected to store data in the chosen key", () => {
+      expect(storedData).to.have.own.property("entries");
     });
 
-    it('is expected to store the data as a string', () => {
-      expect(typeof storedData.entries).to.equal("string")
+    it("is expected to store the data as a string", () => {
+      expect(typeof storedData.entries).to.equal("string");
+    });
+  });
+
+  describe("#ggetItem", () => {
+    context("existing key", () => {
+      def("response", () => $subject.getItem("entries"));
+
+      before(() => {
+        $subject.setItem("entries", JSON.stringify(entry1));
+      });
+
+      it("is expecting to return a string", () => {
+        expect(typeof $response).to.equal("string");
+      });
+
+      it("is expected to contain the data", () => {
+        expect(JSON.parse($response)).to.eql(entry1);
+      });
+    });
+
+    context("non existing key", () => {
+      def("response", () => $subject.getItem("nonExistingKey"));
+
+      it('is expected to return "undefined"', () => {
+        expect($response).to.eq(undefined);
+      });
+    });
+  });
+
+  describe("#removeItem", () => {
+    beforeEach(() => {
+      $subject.setItem("keyToRemove", "some value");
+    });
+
+    it("is expected to remove an item", () => {
+      $subject.removeItem("keyToRemove");
+      expect($subject.data).to.not.have.own.property("keyToRemove");
+    });
+  });
+
+  describe("#clear", () => {
+    beforeEach(() => {
+      $subject.setItem("myKey", "some value");
+      $subject.setItem("myKey2", "some value");
+      $subject.clear();
+    });
+
+    it('is expected to remove an item myKey', () => {
+      expect($subject.data).to.not.have.own.property("myKey")
+    });
+
+    it('is expected to remove an item myKey2', () => {
+      expect($subject.data).to.not.have.own.property("myKey2")
     });
   });
 });
